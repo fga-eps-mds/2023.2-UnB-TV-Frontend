8f2374219676cc07915725f527716c95ca0a50c5
João Victor <joao15victor08@gmail.com>
2023-10-21 19:11:57 -0300

    Altera src da url para utilizar safe pipe
    Co-authored-by: Gabriela Tiago <gabrielatiagodearaujo@outlook.com>
    Co-authored-by: Geraldo Victor <geraldovictor@outlook.com>

--- src/app/pages/video/video-viewer.component.ts
+++ src/app/pages/video/video-viewer.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpResponse } from '@angular/common/http';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import {
   IVideoVersion,
   VideoVersion,
@@ -11,21 +10,22 @@ import {
   VideoDetails,
 } from 'src/shared/model/video-details.model';
 import { VideoService } from './video.service';
+
+
 @Component({
   selector: 'app-video-viewer',
   templateUrl: './video-viewer.component.html',
   styleUrls: ['./video-viewer.component.css'],
 })
+
 export class VideoViewerComponent implements OnInit {
   videoVersion: IVideoVersion = new VideoVersion();
   videoMP4: IVideoDetails = new VideoDetails();
-  videoUrl: SafeResourceUrl = '';
 
   constructor(
     private route: ActivatedRoute,
     private videoService: VideoService,
-    private sanitizer: DomSanitizer
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
@@ -38,12 +38,7 @@ export class VideoViewerComponent implements OnInit {
             (video) => video.fileFormat === 'MP4'
           );
 
-          if (!!videoMP4Found) {
-            this.videoMP4 = videoMP4Found;
-            this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
-              this.videoMP4.url as string
-            );
-          }
+          this.videoMP4 = !!videoMP4Found ? videoMP4Found : this.videoMP4;
         });
     });
   }
