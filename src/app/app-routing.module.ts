22b0c785255f96660b216196fa25ad7afff0a5a0
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-10-21 16:32:18 -0300

    Cria rotas dos componetes de video e video-viewer
    Co-authored-by: João Victor <joao15victor08@gmail.com>
    Co-authored-by: Geraldo Victor <geraldovictor@outlook.com>
    Co-authored-by: Eric Akio <freedom.geek7@gmail.com>
    Co-authored-by: Marcos Castilhos <221008300@aluno.unb.br>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { VideoComponent } from './pages/video/video.component';
+import { VideoViewerComponent } from './pages/video/video-viewer.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'videos', component: VideoComponent },
+  { path: 'video/:idVideo', component: VideoViewerComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
