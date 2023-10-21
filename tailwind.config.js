21e7f6f7627b025df2414a8d985563d7d6b6d90e
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-10-21 11:48:15 -0300

    Adiciona fonte padrão Unb Pro
    Co-authored-by: João Victor <joao15victor08@gmail.com>
    Co-authored-by: Geraldo Victor <geraldovictor@outlook.com>
    Co-authored-by: Eric Akio <freedom.geek7@gmail.com>
    Co-authored-by: Marcos Castilhos <221008300@aluno.unb.br>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ["unb-pro", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
