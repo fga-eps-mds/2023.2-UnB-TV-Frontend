1be8eab620cf7d28f7cbc2df5eb3ac309b79aeec
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-10-21 17:44:24 -0300

    Adiciona novo atributo na model de VideoDetails
    Co-authored-by: João Victor <joao15victor08@gmail.com>
    Co-authored-by: Geraldo Victor <geraldovictor@outlook.com>
    Co-authored-by: Eric Akio <freedom.geek7@gmail.com>
    Co-authored-by: Marcos Castilhos <221008300@aluno.unb.br>

--- src/shared/model/video-details.model.ts
+++ src/shared/model/video-details.model.ts
@@ -1,8 +1,13 @@
 export interface IVideoDetails {
   id?: number;
+  fileFormat?: string;
   url?: string;
 }
 
 export class VideoDetails implements IVideoDetails {
-  constructor(public id?: number, public url?: string) {}
+  constructor(
+    public id?: number,
+    public fileFormat?: string,
+    public url?: string
+  ) {}
 }
