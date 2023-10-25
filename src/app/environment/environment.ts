c84870a14c028bf5810656a251f0fa26c9732ebd
DaviMarinho <davii_marinho@hotmail.com>
2023-10-25 18:28:50 -0300

    Changes

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiURL: 'http://localhost:8000/api',
+  apiURL: 'https://unb-tv-backend-2be1ed3a0485.herokuapp.com/',
   EDUPLAY_CLIENT_KEY: 'a1cdba06226408fcda63b49c50223c68d56005d234cc98bcdc1ae787d2b4de1d',
 };
