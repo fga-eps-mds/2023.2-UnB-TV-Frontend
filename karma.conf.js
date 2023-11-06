543f8b5a4f9b62eec3524a0cec8df1265f6f4b15
davimarinho <davii_marinho@hotmail.com>
2023-11-06 15:12:35 -0300

    Fix ci/cd
    Signed-off-by: davimarinho <davii_marinho@hotmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,15 @@ module.exports = function(config) {
     config.set({
       basePath: '../..',
       frameworks: ['jasmine'],
-      //...
-    });
+      browsers: [
+        'Chrome',
+        'ChromeHeadlessCI'
+    ],
+    customLaunchers: {
+        ChromeHeadlessCI: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox']
+        }
+    },
+    })
   };
