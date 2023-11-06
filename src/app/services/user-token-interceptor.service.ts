a7b778577ed34f018613580a516346bb282a8e71
João Victor <joao15victor08@gmail.com>
2023-11-06 10:40:18 -0300

    Altera interceptor apenas para apis proprias

--- src/app/services/user-token-interceptor.service.ts
+++ src/app/services/user-token-interceptor.service.ts
@@ -12,13 +12,15 @@ import {
 export class UserTokenInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler) {
     const token = localStorage.getItem('token');
-
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+    console.log(request.url);
+    if (request.url.includes("localhost")) {
+      if (token) {
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      }
     }
 
     return next.handle(request);
