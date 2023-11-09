6a9c8cd3f61d4036022a3023a29b74bea17a9081
Ricardo Loureiro <ricardoloureiro75@gmail.com>
2023-11-09 11:18:22 -0300

    corrections lgoin sso

--- src/app/pages/login-social/login-social.component.ts
+++ src/app/pages/login-social/login-social.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-social',
   templateUrl: './login-social.component.html',
   styleUrls: ['./login-social.component.css']
 })
-export class LoginSocialComponent {
 
+export class LoginSocialComponent  {
+
+  constructor(private router: Router) {}
+
+  loginWithGoogle() {
+    
+    window.location.href = 'http://localhost:8000/auth/login';
+  }
+
+  loginWithFacebook() {
+    
+    window.location.href = 'http://localhost:8000/facebook';
+  }
 }
