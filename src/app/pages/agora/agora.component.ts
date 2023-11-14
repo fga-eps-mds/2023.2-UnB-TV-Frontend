b6965066369947fa9183783a9c8392464658e873
eric-kingu <freedom.geek7@gmail.com>
2023-11-14 13:50:08 -0300

    Adiciona componentes placeholder para link
    Co-authored-by: Savio Cunha <saviocunha61@gmail.com>

---
+++ src/app/pages/agora/agora.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agora',
+  templateUrl: './agora.component.html',
+  styleUrls: ['./agora.component.css']
+})
+export class AgoraComponent {
+
+}
---
+++ src/app/pages/programacao/programacao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programacao',
+  templateUrl: './programacao.component.html',
+  styleUrls: ['./programacao.component.css']
+})
+export class ProgramacaoComponent {
+
+}
