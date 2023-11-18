914f965bf3d96a9ef6dd886469bd170f8761ecd4
João Victor <joao15victor08@gmail.com>
2023-11-18 15:05:57 -0300

    Corrige teste reset-password

--- src/app/pages/reset-password/reset-password.component.spec.ts
+++ src/app/pages/reset-password/reset-password.component.spec.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../../services/auth.service';
 import { of, throwError } from 'rxjs';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MessageService } from 'primeng/api';
+import { AlertService } from 'src/app/services/alert.service';
 
 class AuthServiceMock {
   constructor() { }
@@ -13,11 +14,21 @@ class AuthServiceMock {
   }
 }
 
+class AlertServiceMock {
+  constructor() { }
+  showMessage() {
+    return of({ success: true });
+  }
+  errorMessage() {
+    return of({ success: true });
+  }
+}
 
 describe('ResetPasswordComponent', () => {
   let component: ResetPasswordComponent;
   let fixture: ComponentFixture<ResetPasswordComponent>;
   let authService: AuthService;
+  let alertService: AlertService;
 
   beforeEach(() => {
 
@@ -25,6 +36,7 @@ describe('ResetPasswordComponent', () => {
       imports: [HttpClientTestingModule, ReactiveFormsModule],
       declarations: [ResetPasswordComponent],
       providers: [
+        { provide: AlertService, useValue: new AlertServiceMock() },
         { provide: AuthService, useValue: new AuthServiceMock() },
         { provide: FormBuilder },
         MessageService
@@ -32,6 +44,7 @@ describe('ResetPasswordComponent', () => {
     });
     fixture = TestBed.createComponent(ResetPasswordComponent);
     authService = TestBed.inject(AuthService);
+    alertService = TestBed.inject(AlertService);
     component = fixture.componentInstance;
   });
 
@@ -67,7 +80,7 @@ describe('ResetPasswordComponent', () => {
   });
 
   it('should show an alert on error', () => {
-    const alertSpy = spyOn(window, 'alert');
+    const alertSpy = spyOn(alertService, 'showMessage').and.callThrough();
     spyOn(component, 'changePassword').and.callThrough();
     fixture.detectChanges();
 
@@ -75,7 +88,7 @@ describe('ResetPasswordComponent', () => {
       'button[type="submit"]'
     );
     submitButton.click();
-    expect(alertSpy).toHaveBeenCalledWith('Preencha todos os campos corretamente!');
+    expect(alertSpy).toHaveBeenCalled();
   });
 
   it('should call changePassword and return an error', () => {
