8618bc2eeb7249f242677a416c14016f0698ed36
geraldovictor <geraldovictor@outlook.com>
2023-11-18 23:42:05 -0300

    Corrige testes que estavam falhando

--- src/app/pages/grid/grid.component.spec.ts
+++ src/app/pages/grid/grid.component.spec.ts
@@ -1,19 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { GridComponent } from './grid.component';
+import { GridService } from 'src/app/services/grid.service';
+import { Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of, throwError } from 'rxjs';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+class GridServiceMock {
+  getSchedule() {
+    return of({ success: true });
+  }
+}
 
 describe('GridComponent', () => {
   let component: GridComponent;
   let fixture: ComponentFixture<GridComponent>;
+  let gridService: GridService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GridComponent ]
+      imports: [ProgressSpinnerModule, HttpClientTestingModule, RouterTestingModule.withRoutes(
+        [
+          { path: 'grid-days/:day', component: GridComponent }
+        ]
+      )],
+      declarations: [GridComponent],
+      providers: [{ provide: GridService, useValue: new GridServiceMock() }]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(GridComponent);
     component = fixture.componentInstance;
+    gridService = TestBed.inject(GridService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
--- src/app/services/grid.service.spec.ts
+++ src/app/services/grid.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GridService } from './grid.service';
 
 describe('GridService', () => {
   let service: GridService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(GridService);
   });
 
