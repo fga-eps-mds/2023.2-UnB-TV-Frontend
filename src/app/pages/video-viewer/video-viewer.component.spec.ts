c5052ddd2bd9bb2ad79a946f9a6911ac7f00a06b
Marcos Castilhos <221008300@aluno.unb.br>
2023-11-19 15:28:33 -0300

    apaga safe pipe

--- src/app/pages/video-viewer/video-viewer.component.spec.ts
+++ src/app/pages/video-viewer/video-viewer.component.spec.ts
@@ -2,12 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { VideoViewerComponent } from './video-viewer.component';
-import { SafePipe } from 'src/app/pipes/safe.pipe';
 import { VideoCommentComponent } from 'src/app/components/video-comment/video-comment.component';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { VideoService } from 'src/app/services/video.service';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 describe('VideoViewerComponent', () => {
   let component: VideoViewerComponent;
@@ -16,7 +15,7 @@ describe('VideoViewerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [VideoViewerComponent, SafePipe, VideoCommentComponent],
+      declarations: [VideoViewerComponent, VideoCommentComponent],
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
