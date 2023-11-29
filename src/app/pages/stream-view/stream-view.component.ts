cd7f1dfef8cee06bcc25e35d1f119dc133e925e4
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-11-29 03:23:10 -0300

    Renderiza informações de data

--- src/app/pages/stream-view/stream-view.component.ts
+++ src/app/pages/stream-view/stream-view.component.ts
@@ -3,7 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stream-view',
   templateUrl: './stream-view.component.html',
-  styleUrls: ['./stream-view.component.css']
+  styleUrls: ['./stream-view.component.css'],
 })
 export class StreamViewComponent {
+  date: Date = new Date();
+  weekDay: string = '';
+  todaysDate: string = '';
+
+  ngOnInit(): void {
+    this.getTodaysDateInfos();
+  }
+
+  getTodaysDateInfos(): void {
+    const locale: string = 'pt-Br';
+
+    this.weekDay = this.date.toLocaleDateString(locale, { weekday: 'long' });
+    this.weekDay = this.capitalizeFirstLetter(this.weekDay);
+
+    this.todaysDate = this.date.toLocaleDateString(locale);
+  }
+
+  capitalizeFirstLetter(value: string): string {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  }
 }
