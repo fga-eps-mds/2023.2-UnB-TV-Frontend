88e5c510680bbc26e71dfa955bda3915605fb799
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-11-29 03:24:03 -0300

    Testa componente de rendizar data

--- src/app/pages/stream-view/stream-view.component.spec.ts
+++ src/app/pages/stream-view/stream-view.component.spec.ts
@@ -8,9 +8,8 @@ describe('StreamViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StreamViewComponent ]
-    })
-    .compileComponents();
+      declarations: [StreamViewComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(StreamViewComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,41 @@ describe('StreamViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Weekday formatting', () => {
+    it('should capitalize the first letter', () => {
+      const mockString = 'test';
+
+      const result = component.capitalizeFirstLetter(mockString);
+
+      expect(result).toBe('Test');
+    });
+
+    it('should keep the first letter capitalized', () => {
+      const mockString = 'Test';
+
+      const result = component.capitalizeFirstLetter(mockString);
+
+      expect(result).toBe('Test');
+    });
+
+    it('should return an empty string given an empty input', () => {
+      const mockString = '';
+
+      const result = component.capitalizeFirstLetter(mockString);
+
+      expect(result).toBe('');
+    });
+  });
+
+  describe('Getting date and time information ', () => {
+    it('should get the day and weekday', () => {
+      const mockDate = new Date('2023-11-26T10:00:00.000');
+      component.date = mockDate;
+
+      expect(component.todaysDate).toBe('26/11/2023');
+      expect(component.weekDay).toBe('Domingo');
+    });
   });
 });
