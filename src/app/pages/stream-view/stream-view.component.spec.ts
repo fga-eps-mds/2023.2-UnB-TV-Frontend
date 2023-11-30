323df9cee52c89e41c9d367ddbd55ddc81ced8d3
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-11-30 10:23:20 -0300

    Adiciona testes da grade de programação em 'stream'

--- src/app/pages/stream-view/stream-view.component.spec.ts
+++ src/app/pages/stream-view/stream-view.component.spec.ts
@@ -1,18 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of, throwError } from 'rxjs';
 import { StreamViewComponent } from './stream-view.component';
+import { GridService } from 'src/app/services/grid.service';
+import { Schedule } from 'src/shared/model/schedule.model';
+
+const mockWeekday = 'QUINTA';
+const mockSchedules: Schedule[] = [
+  {
+    time: '08:00',
+    activity: 'ZAPPING',
+  },
+  {
+    time: '08:40',
+    activity: 'INTERPROGRAMAS',
+  },
+  {
+    time: '09:00',
+    activity: 'UnBTV ENTREVISTA',
+  },
+  {
+    time: '09:30',
+    activity: 'INTERPROGRAMAS',
+  },
+  {
+    time: '10:00',
+    activity: 'EXCLUSIVA',
+  },
+  {
+    time: '10:30',
+    activity: 'ALÉM DO LIXO',
+  },
+  {
+    time: '11:00',
+    activity: 'VISCERAL BRASIL',
+  },
+  {
+    time: '11:30',
+    activity: 'CANAL SAÚDE',
+  },
+];
+const mockGrid = { mockWeekday: mockSchedules };
+
+class GridServiceMock {
+  getSchedule() {
+    return of(mockGrid);
+  }
+}
 
 describe('StreamViewComponent', () => {
   let component: StreamViewComponent;
   let fixture: ComponentFixture<StreamViewComponent>;
+  let gridService: GridService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [StreamViewComponent],
+      providers: [{ provide: GridService, useValue: new GridServiceMock() }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(StreamViewComponent);
     component = fixture.componentInstance;
+    gridService = TestBed.inject(GridService);
     fixture.detectChanges();
   });
 
@@ -20,7 +68,7 @@ describe('StreamViewComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('Weekday formatting', () => {
+  describe('Capitalizing the first letter', () => {
     it('should capitalize the first letter', () => {
       const mockString = 'test';
 
@@ -53,6 +101,47 @@ describe('StreamViewComponent', () => {
 
       expect(component.todaysDate).toBe('26/11/2023');
       expect(component.weekDay).toBe('Domingo');
+    });
+  });
+
+  describe('Formatting weekday', () => {
+    it('should remove "-feira" and convert to uppercase', () => {
+      const weekday = 'Quinta-feira';
+
+      const result = component.formatWeekday(weekday);
+
+      expect(result).toBe('QUINTA');
+      expect(result).not.toBe('Quinta-feira');
+    });
+
+    it('should remain unchanged when there is no "-feira"', () => {
+      const weekday = 'Domingo';
+
+      const result = component.formatWeekday(weekday);
+
+      expect(result).toBe('DOMINGO');
+      expect(result).not.toBe('Domingo');
+    });
+
+    it('should remain unchanged when the string is empty', () => {
+      const result = component.formatWeekday('');
+      expect(result).toBe('');
+    });
+  });
+
+  describe("Getting today's schedules", () => {
+    it("should call getTodaysSchedule and return today's schedule", () => {
+      const mySpy = spyOn(gridService, 'getSchedule').and.callThrough();
+      component.getTodaysSchedule();
+      expect(mySpy).toHaveBeenCalled();
+    });
+
+    it('should call getTodaysSchedule and return an error', () => {
+      const mySpy = spyOn(gridService, 'getSchedule').and.returnValue(
+        throwError(() => new Error('Erro'))
+      );
+      component.getTodaysSchedule();
+      expect(mySpy).toHaveBeenCalled();
     });
   });
 });
