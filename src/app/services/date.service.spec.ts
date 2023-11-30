e30e659184db3cef69292084977a188052eb3520
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-11-30 15:08:45 -0300

    Adciona testes  aos métodos de busca e conversão de hora

--- src/app/services/date.service.spec.ts
+++ src/app/services/date.service.spec.ts
@@ -13,9 +13,48 @@ describe('DateService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should return a valid current date', () => {
-    const currentDate = service.getCurrentDate();
+  describe('Getting the current date', () => {
+    it('should return a valid current date', () => {
+      const currentDate = service.getCurrentDate();
 
-    expect(currentDate instanceof Date).toBe(true);
+      expect(currentDate instanceof Date).toBe(true);
+    });
+  });
+
+  describe('Getting the current time', () => {
+    it('should return a valid current time in "HH:mm" format', () => {
+      const currentTime = service.getCurrentTime();
+
+      const timeRegex = /^\d{2}:\d{2}$/;
+      expect(timeRegex.test(currentTime)).toBe(true);
+    });
+  });
+
+  describe('Formatting the time string', () => {
+    it('should pad zero for single-digit hours', () => {
+      const paddedValue = service.padZero(5);
+
+      expect(paddedValue).toBe('05');
+      expect(typeof paddedValue).toBe('string');
+    });
+
+    it('should not pad zero for double-digit hours', () => {
+      const paddedValue = service.padZero(15);
+
+      expect(paddedValue).toBe('15');
+      expect(typeof paddedValue).toBe('string');
+    });
+  });
+
+  describe('Converting a time string to Date', () => {
+    it('should convert time string to a valid Date object', () => {
+      const timeString = '12:30';
+      const date = service.convertTimeStringToDate(timeString);
+
+      expect(date instanceof Date).toBe(true);
+      expect(date.getHours()).toBe(12);
+      expect(date.getMinutes()).toBe(30);
+      expect(date.getSeconds()).toBe(0);
+    });
   });
 });
