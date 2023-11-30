e4e8030589e4cf9223fd48ed9fe8e2f133ce1770
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-11-30 15:05:37 -0300

    Adiciona método para buscar a hora atual

--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -7,4 +7,16 @@ export class DateService {
   getCurrentDate(): Date {
     return new Date();
   }
+
+  getCurrentTime(): string {
+    const currentDate = this.getCurrentDate();
+    const hours = this.padZero(currentDate.getHours());
+    const minutes = this.padZero(currentDate.getMinutes());
+    return `${hours}:${minutes}`;
+  }
+
+  // garantir que os minutos e horas sejam sempre representados com dois dígitos
+  padZero(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
 }
