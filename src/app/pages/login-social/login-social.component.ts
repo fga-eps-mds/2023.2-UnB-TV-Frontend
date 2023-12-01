2367be1a92a8a2f0a007fc902fc1f0a1d605a37c
Ricardo Loureiro <ricardoloureiro75@gmail.com>
2023-12-01 11:14:24 -0300

    Login by facebook

--- src/app/pages/login-social/login-social.component.ts
+++ src/app/pages/login-social/login-social.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
-import { SocialAuthService, SocialUser  } from "@abacritt/angularx-social-login";
-import { FacebookLoginProvider } from "@abacritt/angularx-social-login";
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { FacebookLoginProvider } from '@abacritt/angularx-social-login';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+interface ServerResponse {
+  access_token: string;
+}
 
 @Component({
   selector: 'app-login-social',
@@ -8,28 +14,57 @@ import { FacebookLoginProvider } from "@abacritt/angularx-social-login";
   styleUrls: ['./login-social.component.css']
 })
 export class LoginSocialComponent {
-
   private user: SocialUser | null = null;
   private loggedIn: boolean = false;
-  
-  constructor(private authService: SocialAuthService) {
+
+  constructor(
+    private authService: SocialAuthService,
+    private http: HttpClient,
+    private router: Router
+  ) {
     this.authService.authState.subscribe((user) => {
       this.user = user;
-      this.loggedIn = (user != null);
+      this.loggedIn = user != null;
 
       if (this.loggedIn) {
         console.log('Nome do usuário:', user.name);
         console.log('Email do usuário:', user.email);
       }
     });
-   }
+  }
 
   signInWithFB(): void {
-    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then((user) => {
+      
+      if (user) {
+        this.sendUserDataToServer(user);
+      }
+    });
   }
 
   signOut(): void {
     this.authService.signOut();
   }
 
+  private sendUserDataToServer(user: SocialUser) {
+    const userSocialData = {
+      name: user.name,
+      email: user.email
+    };
+  
+    this.http.post('http://localhost:8000/api/auth/login/social', userSocialData).subscribe(
+      (response) => {
+        console.log('Resposta do servidor:', response);
+        
+        
+        if (response && 'access_token' in response) {
+          
+          this.router.navigate(['/videos']);
+        }
+      },
+      (error) => {
+        console.error('Erro ao enviar dados para o servidor:', error);
+      }
+    );
+  }
 }
