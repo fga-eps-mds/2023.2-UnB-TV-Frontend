346265d25cab7380ea102fe441e9a0a648c251a1
Ricardo Loureiro <ricardoloureiro75@gmail.com>
2023-12-04 13:33:02 -0300

    teste front end login social component

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 export const EDUPLAY_CLIENT_KEY = "a1cdba06226408fcda63b49c50223c68d56005d234cc98bcdc1ae787d2b4de1d";
 export const environment = {
   videoAPIURL: 'https://unb-tv-video-service-2023-2-1246521a8eef.herokuapp.com/api',
-  usersAPIURL: 'http://localhost:8000/api',
+  usersAPIURL: 'https://unb-tv-backend-2be1ed3a0485.herokuapp.com/api',
   adminAPIURL: 'https://admin-unb-tv-2023-2-dc0dd53d3aca.herokuapp.com/api'
 };
--- src/app/pages/login-social/login-social.component.spec.ts
+++ src/app/pages/login-social/login-social.component.spec.ts
@@ -1,17 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginSocialComponent } from './login-social.component';
+import { SocialAuthService } from '@abacritt/angularx-social-login';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../../services/auth.service';
+import { of } from 'rxjs';
 
 describe('LoginSocialComponent', () => {
   let component: LoginSocialComponent;
   let fixture: ComponentFixture<LoginSocialComponent>;
 
+  
+  const mockSocialAuthService = jasmine.createSpyObj('SocialAuthService', ['signIn', 'signOut']);
+  mockSocialAuthService.authState = of({ 
+    email: 'test@example.com',
+    name: 'Test User',
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginSocialComponent ]
+      declarations: [ LoginSocialComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: SocialAuthService, useValue: mockSocialAuthService },
+      ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginSocialComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
