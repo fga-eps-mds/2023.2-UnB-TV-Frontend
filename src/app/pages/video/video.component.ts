5efa59c8c0bde2c80ad68e30ec632df2e95017ea
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-12-09 01:54:59 -0300

    Cria catálogo de videos agruapdos por palavras chave

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { VideoService } from '../../services/video.service';
 import { IVideo } from 'src/shared/model/video.model';
 import { UNB_TV_CHANNEL_ID } from 'src/app/app.constant';
+import { VideosCatalog } from 'src/shared/model/catalog.model';
 
 @Component({
   selector: 'app-video',
@@ -12,6 +13,7 @@ export class VideoComponent implements OnInit {
   unbTvChannelId = UNB_TV_CHANNEL_ID;
   videosEduplay: IVideo[] = [];
   unbTvVideos: IVideo[] = [];
+  catalog: VideosCatalog = new VideosCatalog();
 
   constructor(private videoService: VideoService) {}
 
@@ -29,6 +31,7 @@ export class VideoComponent implements OnInit {
       },
       complete: () => {
         this.filterVideosByChannel(this.videosEduplay);
+        this.videosCatalog(this.unbTvVideos);
       },
     });
   }
@@ -39,6 +42,58 @@ export class VideoComponent implements OnInit {
 
       if (channel)
         if (channel[0].id === this.unbTvChannelId) this.unbTvVideos.push(video);
+    });
+  }
+
+  videosCatalog(videos: IVideo[]): void {
+    videos.forEach((video) => {
+      let keywords = video.keywords;
+      if (keywords) {
+        keywords = keywords.toLowerCase();
+        if (keywords.includes('arte') || keywords.includes('cultura')) {
+          this.catalog.artAndCulture?.push(video);
+          return;
+        } else if (keywords.includes('entrevista')) {
+          this.catalog.interviews?.push(video);
+          return;
+        } else if (keywords.includes('cinema')) {
+          this.catalog.cinema?.push(video);
+          return;
+        } else if (
+          keywords.includes('documentais') ||
+          keywords.includes('documentarios')
+        ) {
+          this.catalog.documentaries.push(video);
+          return;
+        } else if (
+          keywords.includes('pesquisa') ||
+          keywords.includes('ciência') ||
+          keywords.includes('ciências')
+        ) {
+          this.catalog.researchAndScience?.push(video);
+        } else if (keywords.includes('jornalismo')) {
+          this.catalog.journalism.push(video);
+          return;
+        } else if (
+          keywords.includes('culinária') ||
+          keywords.includes('variedades')
+        ) {
+          this.catalog.cookingAndVarieties.push(video);
+          return;
+        } else if (
+          keywords.includes('séries especiais') ||
+          keywords.includes('eleições') ||
+          keywords.includes('floresta de gente') ||
+          keywords.includes('guia do calouro') ||
+          keywords.includes('memórias') ||
+          keywords.includes('arquitetura')
+        ) {
+          this.catalog.specialSeries.push(video);
+          return;
+        } else {
+          this.catalog.unbtv.push(video);
+        }
+      }
     });
   }
 }
---
+++ src/shared/model/catalog.model.ts
@@ -0,0 +1,13 @@
+import { IVideo } from './video.model';
+
+export class VideosCatalog {
+  journalism: IVideo[] = [];
+  artAndCulture: IVideo[] = [];
+  interviews: IVideo[] = [];
+  cinema: IVideo[] = [];
+  documentaries: IVideo[] = [];
+  researchAndScience: IVideo[] = [];
+  specialSeries: IVideo[] = [];
+  unbtv: IVideo[] = [];
+  cookingAndVarieties: IVideo[] = [];
+}
