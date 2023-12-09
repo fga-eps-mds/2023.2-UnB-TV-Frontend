2416eaedcc4fe3772adeed12f3a4cd325e9ebd38
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2023-12-09 10:28:49 -0300

    Organiza vídeos no catálogo de acordo com suas palavras no titulo

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { VideoService } from '../../services/video.service';
 import { IVideo } from 'src/shared/model/video.model';
+import { Catalog } from 'src/shared/model/catalog.model';
 import { UNB_TV_CHANNEL_ID } from 'src/app/app.constant';
-import { VideosCatalog } from 'src/shared/model/catalog.model';
 
 @Component({
   selector: 'app-video',
@@ -13,7 +13,7 @@ export class VideoComponent implements OnInit {
   unbTvChannelId = UNB_TV_CHANNEL_ID;
   videosEduplay: IVideo[] = [];
   unbTvVideos: IVideo[] = [];
-  catalog: VideosCatalog = new VideosCatalog();
+  catalog: Catalog = new Catalog();
 
   constructor(private videoService: VideoService) {}
 
@@ -46,54 +46,142 @@ export class VideoComponent implements OnInit {
   }
 
   videosCatalog(videos: IVideo[]): void {
+    const keywordsCategories = [
+      {
+        keywords: ['fala, jovem'],
+        category: this.catalog.journalism.falaJovem,
+      },
+      {
+        keywords: ['informe unb'],
+        category: this.catalog.journalism.informeUnB,
+      },
+      { keywords: ['zapping'], category: this.catalog.journalism.zapping },
+      {
+        keywords: ['brasil em questão'],
+        category: this.catalog.interviews.brasilEmQuestao,
+      },
+      { keywords: ['diálogos'], category: this.catalog.interviews.dialogos },
+      {
+        keywords: ['tirando de letra'],
+        category: this.catalog.interviews.tirandoDeLetra,
+      },
+      {
+        keywords: ['entrevista'],
+        category: this.catalog.interviews.entrevistas,
+      },
+      {
+        keywords: ['vasto mundo'],
+        category: this.catalog.interviews.vastoMundo,
+      },
+      {
+        keywords: ['vozes diplomáticas'],
+        category: this.catalog.interviews.vozesDiplomaticas,
+      },
+      {
+        keywords: ['explique sua tese'],
+        category: this.catalog.researchAndScience.expliqueSuaTese,
+      },
+      {
+        keywords: ['fazendo ciência'],
+        category: this.catalog.researchAndScience.fazendoCiencia,
+      },
+      {
+        keywords: ['radar da extensão'],
+        category: this.catalog.researchAndScience.radarDaExtencao,
+      },
+      {
+        keywords: ['se liga no pas'],
+        category: this.catalog.researchAndScience.seLigaNoPAS,
+      },
+      {
+        keywords: ['unbtv ciência'],
+        category: this.catalog.researchAndScience.unbTvCiencia,
+      },
+      {
+        keywords: ['universidade pra quê?', 'universidade para quê?'],
+        category: this.catalog.researchAndScience.universidadeParaQue,
+      },
+      {
+        keywords: ['[em]cantos'],
+        category: this.catalog.artAndCulture.emCantos,
+      },
+      {
+        keywords: ['casa do som'],
+        category: this.catalog.artAndCulture.casaDoSom,
+      },
+      { keywords: ['esboços'], category: this.catalog.artAndCulture.esbocos },
+      {
+        keywords: ['exclusiva'],
+        category: this.catalog.artAndCulture.exclusiva,
+      },
+      {
+        keywords: ['floresta de gente'],
+        category: this.catalog.specialSeries.florestaDeGente,
+      },
+      {
+        keywords: ['guia do calouro'],
+        category: this.catalog.specialSeries.guiaDoCalouro,
+      },
+      {
+        keywords: ['memórias sobre paulo freire'],
+        category: this.catalog.specialSeries.memoriasPauloFreire,
+      },
+      {
+        keywords: ['desafios das eleições'],
+        category: this.catalog.specialSeries.desafiosDasEleicoes,
+      },
+      {
+        keywords: ['vida de estudante'],
+        category: this.catalog.specialSeries.vidaDeEstudante,
+      },
+      {
+        keywords: ['arquitetura'],
+        category: this.catalog.specialSeries.arquiteturaICC,
+      },
+      {
+        keywords: [
+          'mini doc',
+          'cerrado de volta',
+          'construção tradicional kalunga',
+          'o muro',
+          'um lugar para onde voltar',
+          'vidas no cárcere',
+        ],
+        category: this.catalog.documentaries.miniDoc,
+      },
+      {
+        keywords: [
+          'documentários',
+          'documentário',
+          'quanto vale um terço?',
+          'refazendo os caminhos de george gardner',
+          'sem hora para chegar',
+          'todas podem ser vitímas',
+        ],
+        category: this.catalog.documentaries.documentaries,
+      },
+      {
+        keywords: ['pitadas do cerrado'],
+        category: this.catalog.varieties.pitadasDoCerrado,
+      },
+    ];
+
     videos.forEach((video) => {
-      let keywords = video.keywords;
-      if (keywords) {
-        keywords = keywords.toLowerCase();
-        if (keywords.includes('arte') || keywords.includes('cultura')) {
-          this.catalog.artAndCulture?.push(video);
-          return;
-        } else if (keywords.includes('entrevista')) {
-          this.catalog.interviews?.push(video);
-          return;
-        } else if (keywords.includes('cinema')) {
-          this.catalog.cinema?.push(video);
-          return;
-        } else if (
-          keywords.includes('documentais') ||
-          keywords.includes('documentarios')
-        ) {
-          this.catalog.documentaries.push(video);
-          return;
-        } else if (
-          keywords.includes('pesquisa') ||
-          keywords.includes('ciência') ||
-          keywords.includes('ciências')
-        ) {
-          this.catalog.researchAndScience?.push(video);
-        } else if (keywords.includes('jornalismo')) {
-          this.catalog.journalism.push(video);
-          return;
-        } else if (
-          keywords.includes('culinária') ||
-          keywords.includes('variedades')
-        ) {
-          this.catalog.cookingAndVarieties.push(video);
-          return;
-        } else if (
-          keywords.includes('séries especiais') ||
-          keywords.includes('eleições') ||
-          keywords.includes('floresta de gente') ||
-          keywords.includes('guia do calouro') ||
-          keywords.includes('memórias') ||
-          keywords.includes('arquitetura')
-        ) {
-          this.catalog.specialSeries.push(video);
-          return;
+      const keywordsTitle = video?.title?.toLowerCase() ?? '';
+
+      if (keywordsTitle) {
+        const category = keywordsCategories.find((config) =>
+          config.keywords.some((keyword) => keywordsTitle.includes(keyword))
+        );
+
+        if (category) {
+          category.category.push(video);
         } else {
           this.catalog.unbtv.push(video);
         }
       }
     });
+
+    console.log(this.catalog);
   }
 }
