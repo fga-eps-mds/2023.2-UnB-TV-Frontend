6f578dac900189f02b4a18f6b98f35a26ba3bf04
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2023-12-10 21:12:48 -0300

    Cria componente de confirmação visual para conteúdo não disponível

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { VideoService } from '../../services/video.service';
 import { IVideo } from 'src/shared/model/video.model';
 
@@ -10,7 +11,7 @@ import { IVideo } from 'src/shared/model/video.model';
 export class VideoComponent implements OnInit {
   unbTvVideos: IVideo[] = [];
 
-  constructor(private videoService: VideoService) {}
+  constructor(private videoService: VideoService, private router: Router) {}
 
   ngOnInit(): void {
     this.getVideos();
@@ -25,5 +26,9 @@ export class VideoComponent implements OnInit {
         console.error(error);
       },
     });
+  }
+
+  returnToCatalog(): void {
+    this.router.navigate(['/catalog']);
   }
 }
